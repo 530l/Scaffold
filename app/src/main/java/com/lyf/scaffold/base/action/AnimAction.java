f30736fde316be5df52e3674dc69f54e71890357
linyingfa <530810890@qq.com>
2022-09-05 18:07:53 +0800

    Initial commit

---
+++ app/src/main/java/com/lyf/scaffold/base/action/AnimAction.java
@@ -0,0 +1,12 @@
+package com.lyf.scaffold.base.action;
+
+
+/**
+ *    author : Android 轮子哥
+ *    github : https://github.com/getActivity/AndroidProject
+ *    time   : 2019/09/21
+ *    desc   : 动画样式
+ */
+public interface AnimAction {
+
+}
---
+++ app/src/main/java/com/lyf/scaffold/ui/activity/HomeActivity.kt
@@ -0,0 +1,17 @@
+package com.lyf.scaffold.ui.activity
+
+import com.lyf.scaffold.R
+import com.lyf.scaffold.base.BaseActivity
+
+class HomeActivity : BaseActivity() {
+    override fun getLayoutId(): Int {
+        return R.layout.activity_home
+    }
+
+    override fun initView() {
+    }
+
+    override fun initData() {
+
+    }
+}
